47cbf05dc23a8e154fb958f1643758eef3840a22
Apoorva Lal <lal.apoorva@gmail.com>
2024-07-22 00:02:41 -0700

    "balancing weights"

--- aipyw/aipyw.py
+++ aipyw/aipyw.py
@@ -1,8 +1,11 @@
+from typing import Tuple
 import numpy as np
 
 from sklearn.base import clone
 from sklearn.linear_model import LogisticRegression, LinearRegression, RidgeCV
 from sklearn.model_selection import KFold
+from scipy import optimize
+from sklearn.preprocessing import MinMaxScaler
 
 
 class AIPyW:
@@ -21,6 +24,7 @@ class AIPyW:
         riesz_method="ipw",
         n_splits=2,
         bal_order=None,
+        bal_obj=None,
     ):
         """
         Initialize the AIpyw class.
@@ -29,9 +33,10 @@ class AIPyW:
         - propensity_model: The model used to estimate the propensity scores. Default is None, which uses LogisticRegression.
         - outcome_model: The model used to estimate the outcome regression. Default is None, which uses LinearRegression.
         - balance_model: The model used to estimate the balance regression. Default is None, which uses RidgeCV.
-        - riesz_method: The method used for Riesz balancing. Can be 'ipw', 'linear', or 'kernel'. Default is 'ipw'.
+        - riesz_method: The method used for Riesz balancing. Can be 'ipw', 'linear', 'kernel', or "balancing". Default is 'ipw'.
         - n_splits: The number of splits for cross-validation. Default is 2.
         - bal_order: The order of balance polynomial under linear balancing weights. Default is None, which uses 2.
+        - bal_obj: The objective function for balancing weights. Default is None, which uses 'entropy'.
         """
 
         self.n_splits = n_splits
@@ -41,6 +46,7 @@ class AIPyW:
         self.balance_model = balance_model or RidgeCV()
         self.riesz_method = riesz_method or "ipw"
         self._bal_order = bal_order or 2
+        self._bal_obj = bal_obj or "quadratic"
 
     def fit(self, X, W, Y, n_rff=None):
         """
@@ -110,7 +116,28 @@ class AIPyW:
                     a_x[test_index, w] = self._linear_balancing(
                         X_train, W_train, X_test, w
                     )
+        elif self.riesz_method == "riesz":
+            raise NotImplementedError("Riesz balancing not yet implemented.")
+        elif self.riesz_method == "balancing":
+            for w in range(self.K):
+                a_x[:, w] = self._balancing(X, w)
+                pass
         return a_x
+
+    def _balancing(self, X, w):
+        scaler = MinMaxScaler()
+        Xw = scaler.fit_transform(X[self.W == w])
+        X_all = scaler.transform(X)
+        # store deviation from the target covariates
+        Z = np.c_[
+            np.ones(Xw.shape[0]),
+            Xw - np.average(X_all, axis=0),
+        ]
+        weight_link, beta, status = balancing_weights(
+            Z, objective=self._bal_obj, min_weight=0.0, max_weight=1.0, l2_norm=0
+        )
+        Xmat = np.c_[np.ones(X_all.shape[0]), X_all]
+        return weight_link(np.dot(Xmat, beta))
 
     def _linear_balancing(self, X_train, W_train, X_test, w):
         model = clone(self.balance_model)
@@ -120,8 +147,9 @@ class AIPyW:
 
     def _riesz_balancing(self, X_train, W_train, X_test, w):
         # !TODO implement Riesz loss
-        pass
+        # α = argmin E [ alpha(W) ** 2 - 2m(D; α)]
 
+        pass
 
     def _cross_fit_outcome(self, X, W, Y):
         kf = KFold(n_splits=self.n_splits, shuffle=True, random_state=42)
@@ -165,3 +193,79 @@ class AIPyW:
                 effect, se = np.mean(psi_ij), np.std(psi_ij) / np.sqrt(self.n)
                 effects[f"{j} vs {i}"] = {"effect": effect, "se": se}
         return effects
+
+
+def balancing_weights(
+    z: np.ndarray,
+    objective: str = "entropy",
+    min_weight: float = 0.0,
+    max_weight: float = 10.0,
+    l2_norm: float = 0,
+) -> Tuple[np.ndarray, bool]:
+    """Calibrates covariates toward target.
+
+    It solves a constrained convex optimization problem that minimizes the
+    variation of weights for units while achieving direct covariate
+    balance. The weighted mean of covariates would match the simple mean
+    of target covariates up to a prespecified L2 norm. If there is no feasible
+    solution of weights to satisfy the covariate balance constraint, it would
+    exit with the boolean optimization status set false, in which case, one needs
+    to relax the covariate constraint by increasing the `l2_norm`
+    There are two choices of the optimization objective: entropy of the weights
+    (entropy balancing, or EB) and effective sample size implied by the weights
+    (quadratic balancing, or QB). EB can be viewed as minimizing the
+    Kullback-Leibler divergence between the optimal weights and equal weights;
+    while QB effectively minimizes the Euclidean distance between the optimal
+    weights and equal weights. The two objectives correspond to different link
+    functions for the weights (or the odds of propensity scores) - `exp(x)` for EB
+    and `max(x, 0)` for QB. Therefore, EB weights are strictly positive; while QB
+    weights can be zero and induce sparsity.
+
+    Args:
+      z : Matrix of starting weights. X0 - X1bar
+      objective: The objective of the convex optimization problem. Supported
+        values are "entropy" and "quadratic".
+      min_weight: The lower bound on weights. Must be between 0.0 and the uniform
+        weight (1 / number of rows in `covariates`).
+      max_weight: The upper bound on weights. Must be between the uniform weight
+        (1 / number of rows in `covariates`) and 1.0.
+      l2_norm: The L2 norm of the covaraite balance constraint, i.e., the
+        Euclidean distance between the weighted mean of covariates and the simple
+        mean of target covaraites after balancing.
+    """
+    n, k = z.shape
+    k -= 1
+    if objective == "entropy":
+        weight_link = lambda x: np.exp(np.minimum(x, np.log(1e8)))
+        beta_init = np.zeros(k + 1)
+    elif objective == "quadratic":
+        weight_link = lambda x: np.clip(x, min_weight, max_weight)
+        beta_init = np.linalg.pinv(z.T @ z) @ np.concatenate((np.ones(1), np.zeros(k)))
+
+    def estimating_equation(beta):
+        weights = weight_link(np.dot(z, beta))
+        norm = np.linalg.norm(beta[1:])
+        if norm == 0.0:
+            slack = np.zeros(len(beta[1:]))
+        else:
+            slack = l2_norm * beta[1:] / norm
+        return np.dot(z.T, weights) + np.concatenate((-np.ones(1), slack))
+
+    beta, info_dict, status, msg = optimize.fsolve(
+        estimating_equation, x0=beta_init, full_output=True
+    )
+    weights = weight_link(np.dot(z, beta))
+    # ebal: recompute weight if constraints violated
+    if objective == "entropy" and (
+        (np.max(weights) > max_weight) or (np.min(weights) < min_weight)
+    ):
+        if min_weight == 0.0:
+            weight_link = lambda x: np.exp(np.minimum(x, np.log(max_weight)))
+        else:
+            weight_link = lambda x: np.exp(
+                np.clip(x, np.log(min_weight), np.log(max_weight))
+            )
+        beta, info_dict, status, msg = optimize.fsolve(
+            estimating_equation, x0=beta, full_output=True
+        )
+    return weight_link, beta, status
